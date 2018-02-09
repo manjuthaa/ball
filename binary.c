8e7763dfc444a3afdb892d2aa50d402cf49ab3cc
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-02-09 15:07:59 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int s;
+	scanf("%d",&s);
+	printf("%d",s);
+	if((s==0)||(s==1))
+	{
+	printf("yes it is binary representation");
+	}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
